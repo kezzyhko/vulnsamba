00627b95e4c6fc97d8797e89dd937e9a97f3be60
Sergey Semushin <s.semushin@innopolis.university>
2021-03-13 22:50:21 +0300

    Added smb.conf

--- install-samba.sh
+++ install-samba.sh
@@ -3,6 +3,7 @@ apt-get update
 apt-get install curl make gcc -y
 
 # downloading samba sources
+cd /workdir
 curl https://download.samba.org/pub/samba/stable/samba-3.4.5.tar.gz -o samba-3.4.5.tar.gz
 tar -xvzf samba-3.4.5.tar.gz
 
@@ -11,3 +12,7 @@ cd ./samba-3.4.5/source3/
 ./configure --prefix=/samba
 make
 make install
+
+# copy config
+cd /workdir
+cp smb.conf /samba/lib/smb.conf
