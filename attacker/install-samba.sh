0002a34885dddb63c693fa7b0d0ca45fae878b03
Sergey Semushin <s.semushin@innopolis.university>
2021-03-14 05:54:12 +0300

    Decided to use patched smbclient for the exploit

---
+++ attacker/install-samba.sh
@@ -0,0 +1,17 @@
+# installing packages
+apt-get update
+apt-get install curl make gcc -y
+
+# downloading samba sources
+cd /workdir
+curl https://download.samba.org/pub/samba/stable/samba-3.4.5.tar.gz -o samba-3.4.5.tar.gz
+tar -xvzf samba-3.4.5.tar.gz
+
+# patch for exploit
+cp client.c ./samba-3.4.5/source3/client/client.c
+
+# building from sources
+cd ./samba-3.4.5/source3/
+./configure --prefix=/samba
+make
+make install
